dc14e45a38abc6b34625dec4f9e83b6fa9e4a2a3
igor <20102236@mail.wit.ie>
2025-09-15 19:49:57 +0100

    functions with generics

---
+++ src/04-generics.ts
@@ -0,0 +1,18 @@
+import {friends, colleagues} from './01-basics'
+import {Friend, Colleague} from './myTypes'
+
+function findMatch<T>( data : T[], criteria: (d: T) => boolean ) : T | undefined {
+    return data.find((criteria))
+}
+
+//console.log(findMatch<Friend>(friends, (f) => f.name.startsWith('Jane')  ))
+//console.log(findMatch<Colleague>(colleagues.current, (c) => c.department === 'Finance'  ))
+
+function sort<T>(data: T[], sorter: (d1: T, d2: T) => number){
+    return data.sort(sorter);
+}
+
+
+console.log(sort<Friend>(friends, (a, b) => a.age - b.age));
+
+console.log(sort<Colleague>(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
